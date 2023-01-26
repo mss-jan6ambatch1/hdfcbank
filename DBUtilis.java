a17d2c14db340a6e2cff647468b4b67508e55ae6
KalyanReddy1508 <vangalakalyanreddy1508@gmail.com>
2023-01-26 21:01:23 +0530

    Updated merge

--- DBUtilis.java
+++ DBUtilis.java
@@ -1,3 +1,7 @@
+
+
+i am updating this line on 26th jan
+This line is updated in dev branch
 this line is updated in master branch
 Hello the file contains database realted code
 having some utilities forr connection 
